b69128b172306e4c30dd930c5a4e07dbc6fe0556
pragati2457 <pragatidubey62051@gmail.com>
2023-04-10 00:19:55 +0530

    displayed detailed show data on the show page

---
+++ src/componnts/Shows/Details.jsx
@@ -0,0 +1,16 @@
+const Details = props => {
+
+const { status, premiered, network} = props;
+
+    return (
+    <div>
+        <p>Status: {status}</p>
+        <p>
+          Premiered {premiered}{!!network && `on ${network.name}`}   
+        </p> 
+        
+</div>
+);
+};
+
+export default Details;
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,6 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
-import { getShowById } from "../API/tvmaze";
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { getShowById } from '../API/tvmaze';
+import Details from '../componnts/Shows/Details';
+import ShowMainData from '../componnts/Shows/ShowMainData';
+import Seasons from '../componnts/Shows/Seasons';
+import Cast from '../componnts/Shows/Cast';
 
 
 
@@ -9,16 +13,46 @@ const Show = () => {
     const {data: showData, error: showError} = useQuery({
       queryKey: ['show', showId],
       queryFn: () => getShowById(showId),
+      refetchOnWindowFocus : false,
     });
   
 
     if(showError){
-      return <div>We have an Error :{showError.message}</div>
+      return <div>We have an Error: {showError.message}</div>
     }
     if (showData){
-      return <div>Got sho Data : {showData.name}</div>
+      return( 
+      <div>
+        <ShowMainData
+          image = {showData.image}
+          name = {showData.name}
+          rating = {showData.rating}
+          summary = {showData.summary}
+          genres = {showData.genres}
+        />
+
+        <div>
+          <h2>Details</h2>
+          <Details
+            status = { showData.status}
+            premiered =  {showData.premiered}
+            network = { showData.network}
+          />
+        </div>
+
+        <div>
+          <h2>Seasons</h2>
+          <Seasons seasons={showData._embedded.seasons} />
+        </div>
+
+        <div>
+          <h2>Cast</h2>
+          <Cast cast = {showData._embedded.cast} />
+        </div>
+        </div>
+      );
     }
-    return <div>Show page for show {showId}</div>;
+    return <div>Data is loading</div>;
   };
   
   export default Show;
