46fa915ba460e8ed540a9003c9420e51b72c4e6c
pragati2457 <pragatidubey62051@gmail.com>
2023-04-11 20:44:44 +0530

    created usePersistedReducer to sync state with localStorage

--- src/componnts/Shows/ShowGrid.jsx
+++ src/componnts/Shows/ShowGrid.jsx
@@ -1,6 +1,47 @@
+import { useEffect, useReducer } from "react";
 import ShowCard from "./ShowCard";
 
+const usePersistedReducer = (reducer, initialState, localStorageKey) => {
+  const [ state, dispatch] = useReducer(reducer, initialState, initial => {
+   const persistedValue = localStorage.getItem(localStorageKey);
+
+    return persistedValue ? JSON.parse(persistedValue) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(state));
+  }, [state, localStorageKey]
+  );
+
+  return [state, dispatch]
+}
+
+const starredShowsReducer = (currentStarred, action) => {
+
+  switch (action.type) {
+    case 'STAR':
+      return currentStarred.concat(action.showId);
+    case 'UNSTAR' :
+      return currentStarred.filter(showId => showId !== action.showId);
+    default:
+      return currentStarred;    
+  }
+}
+
 const ShowGrid = ({ shows }) => {
+
+  const [starredShows, dispatchStarred] =  usePersistedReducer(starredShowsReducer, [], 'starredShows');
+
+  const onStarMeClick = showwId => {
+    const isStarred = starredShows.includes(showwId);
+
+    if(isStarred){
+      dispatchStarred({ type: 'UNSTAR', showwId});
+    } else {
+      dispatchStarred({ type: 'STAR', showwId});
+    }
+  }
+
   return (
     <div>
       {shows.map(data => {
@@ -15,6 +56,7 @@ const ShowGrid = ({ shows }) => {
                 : '/public/image-not-found.png'
             }
             summary={data.show.summary}
+            onStarMeClick={onStarMeClick}
           />
         );
       })}
